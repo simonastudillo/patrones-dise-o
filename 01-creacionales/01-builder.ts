4093287230cab8bb1dec9eb1708a830438b6c1dd
Simon Astudillo <simon.astudillo@raef.cl>
2025-06-10 23:09:38 -0300

    08.Builder - Patrón

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,57 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+class Computer {
+
+   public cpu: string = 'cpu - not defined';
+   public ram: string = 'ram - not defined';
+   public storage: string = 'storage - not defined';
+   public gpu?: string = 'gpu - not defined';
+
+   displayConfiguration(): void {
+
+      console.log(`Configuración de la computadora:
+         CPU: ${this.cpu}
+         RAM: ${this.ram}
+         Storage: ${this.storage}
+         GPU: ${this.gpu}
+      `);
+
+   }
+
+}
+
+class ComputerBuilder {
+   private computer: Computer;
+
+   constructor() {
+      this.computer = new Computer();
+   }
+
+   setCPU(cpu: string): ComputerBuilder {
+      this.computer.cpu = cpu;
+      return this;
+   }
+
+   setRAM(ram: string): ComputerBuilder {
+      this.computer.ram = ram;
+      return this;
+   }
+
+   setStorage(storage: string): ComputerBuilder {
+      this.computer.storage = storage;
+      return this;
+   }
+
+   setGPU(gpu: string): ComputerBuilder {
+      this.computer.gpu = gpu;
+      return this;
+   }
+
+   build(): Computer {
+      return this.computer;
+   }
+}
+
+
