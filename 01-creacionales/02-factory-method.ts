27f066e086b6bae6100c063ffbaee729ab7065cd
Simon Astudillo <simon.astudillo@raef.cl>
2025-06-11 22:38:49 -0300

    13. Factory Method - Patrón

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,52 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+interface Hamburger {
+   prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+   prepare(): void {
+      console.log('Preparando hamburguesa de %cpollo',COLORS.yellow);
+   }
+}
+
+class BeefHamburger implements Hamburger {
+   prepare(): void {
+      console.log('Preparando hamburguesa de %cres',COLORS.brown);
+   }
+}
+
+
+abstract class Restaurant {
+   abstract createHamburger(): Hamburger;
+
+   orderHamburger(): void {
+      const hamburger = this.createHamburger();
+      hamburger.prepare();
+   }
+}
+
+
+class ChickenRestaurant extends Restaurant {
+   override createHamburger(): Hamburger {
+      return new ChickenHamburger();
+   }
+}
+
+class BeefRestaurant extends Restaurant {
+   override createHamburger(): Hamburger {
+      return new BeefHamburger();
+   }
+}
+
+
+const chickenRestaurant = new ChickenRestaurant();
+chickenRestaurant.orderHamburger();
+
+const beefRestaurant = new BeefRestaurant();
+beefRestaurant.orderHamburger();
