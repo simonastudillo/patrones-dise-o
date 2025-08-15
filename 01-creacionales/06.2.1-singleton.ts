ac68bec412c38c3afac5e2578c1ca3a91c3c008c
Simon Astudillo <spastudilloq@gmail.com>
2025-08-15 00:20:45 -0300

    32.Singleton - Alternativo

--- 01-creacionales/06.2.1-singleton.ts
+++ 01-creacionales/06.2.1-singleton.ts
@@ -1,11 +1,21 @@
 /**
- * ! Singleton:
- * Es un patrón de diseño creacional que garantiza que una clase
- * tenga una única instancia y proporciona un punto de acceso global a ella.
- *
- * * Es útil cuando necesitas controlar el acceso a una única instancia
- * * de una clase, como por ejemplo, en un objeto de base de datos o en un
- * * objeto de configuración.
- *
- * https://refactoring.guru/es/design-patterns/singleton
- */
+   * ! Singleton:
+   * Es un patrón de diseño creacional que garantiza que una clase
+   * tenga una única instancia y proporciona un punto de acceso global a ella.
+   *
+   * * Es útil cuando necesitas controlar el acceso a una única instancia
+   * * de una clase, como por ejemplo, en un objeto de base de datos o en un
+   * * objeto de configuración.
+   *
+   * https://refactoring.guru/es/design-patterns/singleton
+*/
+
+import { configManager } from "./singleton/config-manager.ts";
+
+configManager.setConfig("apiUrl", "https://api.example.com");
+configManager.setConfig("timeout", "500");
+configManager.setConfig("apiKey", "asfa3165");
+
+
+console.log(configManager.getConfig("apiUrl"));
+console.log(configManager.getAllConfigs());
