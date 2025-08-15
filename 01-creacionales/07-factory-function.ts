59a5f76d423edd8a1050b49cedba7d0b6ecb6c02
Simon Astudillo <spastudilloq@gmail.com>
2025-08-15 20:49:03 -0300

    33.Factory Funcion - Patrón

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,9 +1,25 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
- * ! Factory Function
- * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
- * usados posteriormente en el código.
- *
- * * Es útil cuando necesitamos crear objetos o funciones de manera dinámica,
- * * es decir, en tiempo de ejecución y no en tiempo de compilación.
- *
- */
+   * ! Factory Function
+   * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
+   * usados posteriormente en el código.
+   *
+   * * Es útil cuando necesitamos crear objetos o funciones de manera dinámica,
+   * * es decir, en tiempo de ejecución y no en tiempo de compilación.
+   *
+*/
+
+type Language = 'es' | 'en' | 'fr';
+
+function createGreeter(lang: Language): (name: string) => void
+{
+   return function(name: string) {
+      const messages = {
+         es: `Hola, ${name}!`,
+         en: `Hello, ${name}!`,
+         fr: `Bonjour, ${name}!`
+      }
+
+      return console.log(messages[lang],COLORS.red);
+   }
+}
