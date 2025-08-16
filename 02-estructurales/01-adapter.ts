87732f32c2a85c13abaa82e53b18eded5622a6e3
Simon Astudillo <spastudilloq@gmail.com>
2025-08-15 23:29:17 -0300

    39.Adapter - patrón

--- 02-estructurales/01-adapter.ts
+++ 02-estructurales/01-adapter.ts
@@ -1,12 +1,20 @@
 /**
- * ! Patrón Adapter
- *  Permite que objetos con interfaces incompatibles trabajen juntos, también es muy
- *  util para utilizar librerías de terceros en nuestra aplicación sin depender
- *  directamente de ellas.
- *
- * * Es útil cuando se quiere reutilizar una clase que no tiene la interfaz que
- * * necesitamos o cuando queremos crear una capa de abstracción para una librería
- * * de terceros.
- *
- * https://refactoring.guru/es/design-patterns/adapter
- */
+   * ! Patrón Adapter
+   *  Permite que objetos con interfaces incompatibles trabajen juntos, también es muy
+   *  util para utilizar librerías de terceros en nuestra aplicación sin depender
+   *  directamente de ellas.
+   *
+   * * Es útil cuando se quiere reutilizar una clase que no tiene la interfaz que
+   * * necesitamos o cuando queremos crear una capa de abstracción para una librería
+   * * de terceros.
+   *
+   * https://refactoring.guru/es/design-patterns/adapter
+*/
+
+import { LocalLogger } from "./adapter-files/local-logger.ts";
+
+const logger =  new LocalLogger('01-adapter.ts');
+
+logger.writeLog('Este es un mensaje de log normal');
+logger.writeWarning('Este es un mensaje de log de warning');
+logger.writeError('Este es un mensaje de log de ERROR');
