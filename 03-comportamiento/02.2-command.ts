6ab22b4ad517629624283e32c497a84c29b0c327
Simon Astudillo <spastudilloq@gmail.com>
2025-08-24 18:24:09 -0300

    76.Command - Tarea

--- 03-comportamiento/02.2-command.ts
+++ 03-comportamiento/02.2-command.ts
@@ -1,87 +1,105 @@
 /**
- * ! Patrón Command
- * Este patrón encapsula una solicitud como un objeto,
- * lo que le permite parametrizar otros objetos con diferentes solicitudes,
- * encolar solicitudes, o registrar solicitudes, y soporta operaciones que pueden deshacerse.
- *
- * Me gustó mucho la explicación de Refactoring Guru
- * https://refactoring.guru/es/design-patterns/command
- *
- * * Es útil cuando se necesita desacoplar el objeto que invoca
- * * la operación del objeto que sabe cómo realizarla.
- *
- */
+   * ! Patrón Command
+   * Este patrón encapsula una solicitud como un objeto,
+   * lo que le permite parametrizar otros objetos con diferentes solicitudes,
+   * encolar solicitudes, o registrar solicitudes, y soporta operaciones que pueden deshacerse.
+   *
+   * Me gustó mucho la explicación de Refactoring Guru
+   * https://refactoring.guru/es/design-patterns/command
+   *
+   * * Es útil cuando se necesita desacoplar el objeto que invoca
+   * * la operación del objeto que sabe cómo realizarla.
+   *
+*/
 
 import { COLORS } from '../helpers/colors.ts';
 
 // 1. Interfaz Command
 interface Command {
-  execute(): void;
+   execute(): void;
 }
 
 // 2. Clase Receptor - TextEditor
 
 class TextEditor {
-  private text: string = '';
-  private clipboard: string = '';
-  private history: string[] = [];
+   private text: string = '';
+   private clipboard: string = '';
+   private history: string[] = [];
 
-  // Agregar texto al editor
-  type(text: string): void {
-    this.history.push(this.text); // Guardar estado antes de cambiarlo
-    this.text += text;
-  }
+   // Agregar texto al editor
+   type(text: string): void {
+      this.history.push(this.text); // Guardar estado antes de cambiarlo
+      this.text += text;
+   }
 
-  // Copiar el texto actual
-  copy(): void {
-    this.clipboard = this.text;
-    console.log(
-      `Texto copiado al portapapeles: \n%c"${this.clipboard}"`,
-      COLORS.blue
-    );
-  }
+   // Copiar el texto actual
+   copy(): void {
+      this.clipboard = this.text;
+      console.log(
+         `Texto copiado al portapapeles: \n%c"${this.clipboard}"`,
+         COLORS.blue
+      );
+   }
 
-  // Pegar el texto del portapapeles
-  paste(): void {
-    this.history.push(this.text); // Guardar estado antes de pegar
-    this.text += this.clipboard;
-    console.log(`Texto después de pegar: \n%c"${this.text}"`, COLORS.blue);
-  }
+   // Pegar el texto del portapapeles
+   paste(): void {
+      this.history.push(this.text); // Guardar estado antes de pegar
+      this.text += this.clipboard;
+      console.log(`Texto después de pegar: \n%c"${this.text}"`, COLORS.blue);
+   }
 
-  // Deshacer la última acción
-  undo(): void {
-    if (this.history.length > 0) {
-      this.text = this.history.pop()!;
-      console.log(`Texto después de deshacer: \n%c"${this.text}"`, COLORS.blue);
-      return;
-    }
+   // Deshacer la última acción
+   undo(): void {
+      if (this.history.length > 0) {
+         this.text = this.history.pop()!;
+         console.log(`Texto después de deshacer: \n%c"${this.text}"`, COLORS.blue);
+         return;
+      }
 
-    console.log('No hay nada para deshacer.');
-  }
+      console.log('No hay nada para deshacer.');
+   }
 
-  // Mostrar el texto actual
-  getText(): string {
-    return this.text;
-  }
+   // Mostrar el texto actual
+   getText(): string {
+      return this.text;
+   }
 }
 
 // 3. Clases de Comandos Concretos
 class CopyCommand implements Command {
   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+   constructor(editor: TextEditor) {
+      this.editor = editor;
+   }
+
+   execute(): void {
+      this.editor.copy();
+   }
 }
 
 class PasteCommand implements Command {
-  private editor: TextEditor;
+   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+   constructor(editor: TextEditor) {
+      this.editor = editor;
+   }
+
+   execute(): void {
+      this.editor.paste();
+   }
 }
 
 class UndoCommand implements Command {
-  private editor: TextEditor;
+   private editor: TextEditor;
 
-  // TODO: Inyectar el editor en el constructor y el método execute con la acción respectiva
+   constructor(editor: TextEditor) {
+      this.editor = editor;
+   }
+
+   execute(): void {
+      this.editor.undo();
+   }
 }
 
 // 4. Clase Cliente - Toolbar
@@ -89,16 +107,21 @@ class UndoCommand implements Command {
 class Toolbar {
   private commands: Record<string, Command> = {};
 
-  setCommand(button: string, command: Command): void {
-    // TODO: Asignar el comando al botón correspondiente
-  }
+   setCommand(button: string, command: Command): void {
+      this.commands[button] = command;
+   }
 
-  clickButton(button: string): void {
-    //TODO: Ejecutar el comando correspondiente al botón
+   clickButton(button: string): void {
+      //TODO: Ejecutar el comando correspondiente al botón
+      const command = this.commands[button];
+      if (command) {
+         command.execute();
+         return;
+      }
 
-    // TODO: Manejar el caso en que no haya un comando asignado al botón
-    console.error(`No hay un comando asignado al botón "${button}"`);
-  }
+      // TODO: Manejar el caso en que no haya un comando asignado al botón
+      console.error(`No hay un comando asignado al botón "${button}"`);
+   }
 }
 
 // 5. Código Cliente para probar el patrón Command
