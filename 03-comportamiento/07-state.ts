8b06663bb3d6514e76afe7b97139099cd21bac76
Simon Astudillo <spastudilloq@gmail.com>
2025-08-30 18:12:58 -0300

    96.State - Patrón

--- 03-comportamiento/07-state.ts
+++ 03-comportamiento/07-state.ts
@@ -9,6 +9,8 @@
  * https://refactoring.guru/es/design-patterns/state
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * * Objetivo: Implementar el patrón State para simular el funcionamiento
  * * de una máquina expendedora.
@@ -18,3 +20,63 @@
  *  * Entregando Producto,
  * * y su comportamiento varía dependiendo del estado actual.
  */
+
+interface State {
+   name: string;
+   
+   insertMoney(): void;
+   selectProduct(): void;
+   dispenseProduct(): void;
+}
+
+class VendingMachine {
+   private state: State;
+
+   constructor( state: State) {
+      this.state = state;
+   }
+
+   insertMoney() {
+      this.state.insertMoney();
+   }
+
+   selectProduct() {
+      this.state.selectProduct();
+   }
+
+   dispenseProduct() {
+      this.state.dispenseProduct();
+   }
+
+   setState(state: State) {
+      this.state = state;
+      console.log(`Estado cambiado a: %c${state.name}`, COLORS.green);
+   }
+
+   getStateName() {
+      return this.state.name;
+   }
+}
+
+// Estados
+class WaitingForMoney implements State {
+   public name = "Esperando Dinero";
+   private vendingMachine: VendingMachine;
+
+   constructor(vendingMachine: VendingMachine) {
+      this.vendingMachine = vendingMachine;
+   }
+
+   insertMoney() {
+      console.log("Dinero insertado. Ahora puedes seleccionar un producto");
+      this.vendingMachine.setState(new SelectingProduct(this.vendingMachine));
+   }
+
+   selectProduct() {
+      console.log("%cPor favor, inserte dinero primero.", COLORS.red);
+   }
+
+   dispenseProduct() {
+      console.log("%cPor favor, inserte dinero primero.", COLORS.red);
+   }
+}
