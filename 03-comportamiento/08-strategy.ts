9e6a399729d2f0c3970bbce68955de5e1eb6f7d9
Simon Astudillo <spastudilloq@gmail.com>
2025-08-30 21:17:23 -0300

    101.Strategy - Patrón

--- 03-comportamiento/08-strategy.ts
+++ 03-comportamiento/08-strategy.ts
@@ -12,8 +12,58 @@
  * https://refactoring.guru/es/design-patterns/strategy
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * !Objetivo: Explicar el patrón Strategy usando un ejemplo donde varios
  * ! patitos compiten en una carrera y cada uno tiene su propia
  * ! estrategia de movimiento (por ejemplo, nadar, volar o caminar).
  */
+
+interface MovementStrategy {
+   move(): void;
+}
+
+// Estrategia #1 Rápida pero costosa
+class SwimFast implements MovementStrategy {
+   move(): void {
+      console.log('%cEl pato nada rápidamente sobre el agua.', COLORS.blue);
+   }
+}
+
+// Estrategia #2 No tan rápida pero no tan costosa
+class FlyOverWater implements MovementStrategy {
+   move(): void {
+      console.log('%cEl pato vuela elegantemente sobre el agua.', COLORS.pink);
+   }
+}
+
+// Estrategia #3 Lenta pero económica
+class WalkClumsily implements MovementStrategy {
+   move(): void {
+      console.log('%cEl pato camina torpemente por la orilla.', COLORS.green);
+   }
+}
+
+// Consumidor de estrategia
+class Duck {
+   private name: string;
+   private movementStrategy: MovementStrategy;
+
+   constructor(name: string, movementStrategy: MovementStrategy) {
+      this.name = name;
+      this.movementStrategy = movementStrategy;
+
+      console.log(`%c${ name } %clisto para competir`, COLORS.green, COLORS.white);
+   }
+
+   performMove(): void {
+      console.log(`%c${ this.name } %ccomienza a moverse...`, COLORS.green, COLORS.white);
+      this.movementStrategy.move();
+   }
+
+   setMovementStrategy(movementStrategy: MovementStrategy): void {
+      console.log(`%c${ this.name } %cha cambiado su estrategia de movimiento.`, COLORS.green, COLORS.white);
+      this.movementStrategy = movementStrategy;
+   }
+}
